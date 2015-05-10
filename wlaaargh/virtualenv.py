afca85ffb79c42d351913a1adf0101b8a46e91b4
Emil Eriksson <emil.eriksson@codemill.se>
2015-05-10 12:22:40 +0200

    Add documentation to tasks for virtualenv

--- wlaaargh/virtualenv.py
+++ wlaaargh/virtualenv.py
@@ -6,13 +6,16 @@ import os
 
 env.pipcachedir = w(lambda: os.path.join(env.projectroot(), "pipcache"))
 
+
 @task
 def create(directory):
-    return run("virtualenv '%s'" % directory)
+    """ Create a new virtualenv in the given directory """
+    return run("virtualenv -q '%s'" % directory)
 
 
 @task
 def pip(command):
+    """ Use pip from a virtualenv """
     require("virtualenvroot")
     pip = os.path.join(env.virtualenvroot,
                        "bin", "pip")
@@ -21,6 +24,10 @@ def pip(command):
 
 @task
 def install(packages):
+    """ Install packages into the virtualenv
+
+        Uses a local cache to speed up installation.
+    """
     require("pipcachedir")
 
     pip("install -q --download-cache '%s' %s" % (env.pipcachedir(), packages))
